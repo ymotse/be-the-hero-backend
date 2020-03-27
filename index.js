e25a56b736676c8f267dd76dfe8e209f049717d5
Yitshhaq Fukushima <ymotse@gmail.com>
2020-03-27 09:39:06 -0300

    create first route to test.

--- index.js
+++ index.js
@@ -2,4 +2,12 @@ const express = require('express')
 
 const app = new express()
 
+app.get('/', (request, response) => {
+    
+    return response.json({
+        evento: 'Semana Omnistack 11.0',
+        aluno: 'Yitshhaq Fukushima'
+    })
+})
+
 app.listen(3333)
