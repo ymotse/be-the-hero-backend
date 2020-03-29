654e82c9d19af4f9009f0f10a12082aa93ad92a8
Yitshhaq Fukushima <ymotse@gmail.com>
2020-03-29 15:33:31 -0300

    Validating endpoints

--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const { celebrate, Segments, Joi } = require('celebrate')
 
 const OngController = require('./controllers/OngController')
 const IncidentController = require('./controllers/IncidentController')
@@ -7,15 +8,64 @@ const SessionController = require('./controllers/SessionController')
 
 const routes = express.Router()
 
-routes.post('/sessions', SessionController.create)
+//#region /sessions
+routes.post('/sessions', celebrate({
+    [Segments.BODY]: Joi.object().keys({
+        id: Joi.string().required()
+    })
+}), SessionController.create)
+//#endregion /sessions
 
+
+//#region /ongs
 routes.get('/ongs', OngController.list)
-routes.post('/ongs', OngController.create)
 
-routes.get('/incidents', IncidentController.list)
-routes.post('/incidents', IncidentController.create)
-routes.delete('/incidents/:id', IncidentController.delete)
+routes.post('/ongs', celebrate({
+    [Segments.BODY]: Joi.object().keys({
+        name: Joi.string().required(),
+        email: Joi.string().required().email(),
+        whatsapp: Joi.number().required().min(10).max(11),
+        city: Joi.string().required(),
+        uf: Joi.string().required().length(2)
+    })
+}), OngController.create)
+//#endregion /ongs
 
-routes.get('/profile', ProfileController.index)
+
+//#region /incidents
+routes.get('/incidents', celebrate({
+    [Segments.QUERY]: Joi.object().keys({
+        page: Joi.number()
+    })
+}), IncidentController.list)
+
+routes.post('/incidents', celebrate({
+    [Segments.HEADERS]: Joi.object({
+        authorization: Joi.string().required(),
+    }).unknown()
+}), celebrate({
+    [Segments.BODY]: Joi.object().keys({
+        title: Joi.string().required(),
+        description: Joi.string().required(),
+        value: Joi.number().required().min(1),
+    })
+}), IncidentController.create) //TODO
+
+routes.delete('/incidents/:id', celebrate({
+    [Segments.PARAMS]: Joi.object().keys({
+        id: Joi.number().required()
+    })
+}), IncidentController.delete)
+//#endregion /incidents
+
+
+//#region /profile
+routes.get('/profile', celebrate({
+    [Segments.HEADERS]: Joi.object({
+        authorization: Joi.string().required(),
+    }).unknown()
+}), ProfileController.index)
+//#endregion /profile
+
 
 module.exports = routes
